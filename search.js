132e5c512ecc57bea3c63cf34a76d7a091db39fc
Lucas Silva <l.vinicius.0312@gmail.com>
2024-01-29 04:22:20 -0300

    APi de Pesquisa com JavaScript e ajustes

---
+++ search.js
@@ -0,0 +1,36 @@
+const resultArtist = document.getElementById("result_artist");
+const resultPlaylist = document.getElementById("result_playlists");
+const searchInput = document.getElementById("search_input");
+
+function requestApi(searchTerm) {
+  const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+  fetch(url)
+    .then((response) => response.json())
+    .then((results) => displayResults(results));
+}
+
+function displayResults(results) {
+  resultPlaylist.classList.add('hidden');
+  const artistImage = document.getElementById("artist_image");
+  const artistName = document.getElementById("artist_name");
+
+  results.forEach(element => {
+    artistImage.src = element.urlImg;
+    artistName.innerText = element.name;
+  });
+  resultArtist.classList.remove("hidden");
+}
+
+function hidePlaylists() {
+  playlistContainer.classList.add("hidden");
+}
+
+document.addEventListener("input", function () {
+  const searchTerm = searchInput.value.toLowerCase();
+  if (searchTerm === "") {
+    resultArtist.classList.add("hidden");
+    playlistContainer.remove("hidden");
+    return;
+  }
+  requestApi(searchTerm);
+});
